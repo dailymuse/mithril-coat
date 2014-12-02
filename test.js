0e7d8b3541e76cfad569a76916e8abcbb16f651a
Yusuf Simonson <simonson@gmail.com>
2014-12-02 16:46:41 -0500

    Improvements to template example

--- test.js
+++ test.js
@@ -2,11 +2,15 @@ shadow.registerTemplate("test", function(view, controller) {
 	return ([
 		(function() {
 			if(true) {
-				return (shadow.m("ul", {}, shadow.m("each", {"expr":"users","as":"user"}, shadow.m("li", {}, user))));
+				return (shadow.m("ul", {}, shadow.map(controller.users, function() {
+					return (shadow.m("li", {}, user));
+				})));
 			} else if(false) {
 				return (" should never show ");
 			} 
 		}).call(this),
+		shadow.trust("&nbsp"),
+		shadow.trust('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'),
 		shadow.templates["foo"](view, controller)
 	]);
 });
