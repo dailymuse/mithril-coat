1a146748f95143db13ebfda950da7a0f1e305f14
Shlomo Dalezman <sdalezman@gmail.com>
2014-12-11 08:18:37 -0500

    updated build tool for watchify to take into account new files

--- watch/src.js
+++ watch/src.js
@@ -1,12 +1,11 @@
-var watchify = require('watchify');
-var fromArgs = require('watchify/bin/args');
+var chokidar = require("chokidar");
 var fs = require("fs");
 var path = require("path");
 var mkdirp = require("mkdirp");
+// in order to grab up to date browserify
 var browserify = require("../node_modules/watchify/node_modules/browserify/index.js");
 var watchify = require("watchify");
 var coffeeify = require("coffeeify");
-var stdin = process.stdin;
 
 var argv = require("optimist")
     .usage("Loops over a directory and Watchify's all files in that directory")
@@ -20,132 +19,47 @@ var argv = require("optimist")
 
     .argv;
 
-function isDirectory(file) {
-    try {
-        console.log(fs.lstatSync(file).isDirectory());
-        return fs.lstatSync(file).isDirectory();
-    } catch(e) {
-        console.log('e')
-        return false;
+function run(argv) {
+    if(argv._.length < 2) {
+        console.error("Input and output file/directory arguments required");
+        process.exit(-1);
     }
-}
 
-function replaceExtension(filepath, expectedExtension, newExtension) {
-    var dirpath = path.dirname(filepath);
-    var filename = path.basename(filepath, expectedExtension) + newExtension;
-    return path.join(dirpath, filename);
-}
-
-function walkDirectory(root, callback) {
-    var results = [];
-
-    fs.readdir(root, function(err, files) {
-        if(err) {
-            return callback(err);
-        }
-
-        var pending = files.length;
-
-        if(!pending) {
-            return callback(null, results);
-        }
-
-        files.forEach(function(fileName) {
-            var file = path.join(root, fileName);
-
-            fs.lstat(file, function(err, stat) {
-                if(stat && stat.isDirectory()) {
-                    walkDirectory(file, function(err, subResults) {
-                        if(err) {
-                            return callback(null, results);
-                        }
-
-                        results = results.concat(subResults);
-                        if(!--pending) callback(null, results);
-                    });
-                } else {
-                    results.push(file);
-                    if(!--pending) callback(null, results);
-                }
-            });
-        });
-    });
-}
-
-function run(input, output) {
-    var results = [];
+    var input = argv._[0];
+    var output = argv._[1];
 
     if(!fs.existsSync(input)) {
         console.error("File does not exist:", input);
         process.exit(1);
     }
 
-    if(isDirectory(input)) {
-        //Input is a directory
+    // ignoring .ds_store files
+    var watcher = chokidar.watch(input, { ignored: /\.DS_Store/ });
 
-        if(!isDirectory(output)) {
-            console.log(output)
-            //Input is a directory but output is not: error
-            console.error("Output does not exist or is not a directory");
-            process.exit(1);
-        } else {
-            //Input and output are directories
-            bindDirectory(input, output, results);
-
-            // without this, we would only get streams once enter is pressed
-            stdin.setRawMode( true );
-
-            // resume stdin in the parent process (node app won't quit all by itself
-            // unless an error or process.exit() happens)
-            stdin.resume();
-
-            // i don't want binary, do you?
-            stdin.setEncoding( 'utf8' );
-
-            // on any data into stdin
-            stdin.on( 'data', function( key ){
-                // ctrl-c ( end of text )
-                if ( key === '\u0003' ) {
-                    results.forEach(function(result) {
-
-                        console.log('closing process');
-                        console.log(result);
-                        result.close();
-                    });
-                    process.exit();
-                }
-            });
-
-        }
-    } 
+    watcher.on("add", function(inputPath) {
+        addPath(input, inputPath, output);
+    });
 }
 
-function bindDirectory(inputDirectory, outputDirectory, results) {
-    //Input and output are directories and we want to compile
-    walkDirectory(inputDirectory, function(err, results) {
+function addPath(input, inputFile, output) {
+    var outputFile = replaceExtension(path.join(output, path.relative(input, inputFile)), ".coffee", ".js");
+
+    var parentDirectoryPath = path.join(outputFile, "..");
+
+    mkdirp(parentDirectoryPath, function(err) {
         if(err) {
-            console.error("Could not browse input directory:", err);
-            process.exit(1);
-        } else {
-            results.forEach(function(inputFile) {
-                if(!/\.coffee$/.test(inputFile)) {
-                    return;
-                }
-
-                var outputFile = replaceExtension(path.join(outputDirectory, path.relative(inputDirectory, inputFile)), ".coffee", ".js");
-                var parentDirectoryPath = path.join(outputFile, "..");
-
-                mkdirp(parentDirectoryPath, function(err) {
-                    if(err) {
-                        console.error("Could not create parent directory `" + parentDirectoryPath + "`:", err);
-                        return;
-                    }
-
-                    results.push(watchifyFile(inputFile, outputFile));
-                });
-            });
+            console.error("Could not create parent directory `" + parentDirectoryPath + "`:", err);
+            return;
         }
+
+        watchifyFile(inputFile, outputFile);
     });
+}
+
+function replaceExtension(filepath, expectedExtension, newExtension) {
+    var dirpath = path.dirname(filepath);
+    var filename = path.basename(filepath, expectedExtension) + newExtension;
+    return path.join(dirpath, filename);
 }
 
 function watchifyFile(inputPath, outputPath) {
@@ -176,11 +90,6 @@ function bundleShare(b, outputPath) {
         .pipe(fs.createWriteStream(outputPath));
 }
 
-if(argv._.length < 2) {
-    console.error("Input and output file/directory arguments required");
-    process.exit(-1);
-}
-
-run(argv._[0], argv._[1])
+run(argv)
 
 
