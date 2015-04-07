0a70efc02ad8603eb460a94fa9aa6bc7ac249535
Shlomo Dalezman <sdalezman@gmail.com>
2015-04-07 18:19:23 -0400

    update test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ module.exports = function(view, state) {
 	return ([
 		(function() {
 			if(true) {
-				return (coat.m("ul", {}, coat.map(controller.users, function() {
+				return (coat.m("ul", {}, coat.map(controller.users, function(user, key) {
 					return (coat.m("li", {}, user));
 				})));
 			} else if(false) {
@@ -11,10 +11,10 @@ module.exports = function(view, state) {
 		}).call(this),
 		coat.trust("&nbsp"),
 		coat.trust("'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'"),
-		require("undefined.js")(view, state),
+		require("foo.js")(view, state),
 		(function() {
 			var newView = new view.ButtonView({state: state.subModel});
 			return (coat.m('div', {config: coat.captureEvents(newView)}, newView.render()));
-		}).call(this),
+		}).call(this)
 	]);
 };
